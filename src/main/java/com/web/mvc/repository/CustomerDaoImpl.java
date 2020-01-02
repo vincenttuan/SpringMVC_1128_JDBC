c7cbf6fb416439046d0ccfe2feb782b5ceeaca8b
MB-Teacher <MB-Teacher@MB-207-teacher>
2020-01-02 20:50:26 +0800

    jdbc

--- src/main/java/com/web/mvc/repository/CustomerDaoImpl.java
+++ src/main/java/com/web/mvc/repository/CustomerDaoImpl.java
@@ -12,14 +12,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository(value = "customerDao")
 public class CustomerDaoImpl implements CustomerDao {
-    
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
-    @Override
-    public List<Customer> queryCustomer() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
     @Override
     public List<DiscountCode> queryDiscountCode() {
@@ -28,8 +23,38 @@ public class CustomerDaoImpl implements CustomerDao {
     }
 
     @Override
+    public DiscountCode getDiscountCode(String code) {
+        String sql = "SELECT * FROM DISCOUNT_CODE WHERE DISCOUNT_CODE = ?";
+        DiscountCode dc = jdbcTemplate.queryForObject(sql, new Object[]{code}, RM.discountCode);
+        return dc;
+    }
+    
+    @Override
+    public void saveDiscountCode(DiscountCode dc) {
+        String sql = "INSERT INTO DISCOUNT_CODE(DISCOUNT_CODE, RATE) VALUES(?, ?)";
+        jdbcTemplate.update(sql, dc.getDiscountCode(), dc.getRate());
+    }
+
+    @Override
+    public void updateDiscountCode(DiscountCode dc) {
+        String sql = "UPDATE DISCOUNT_CODE SET RATE = ? WHERE DISCOUNT_CODE = ?";
+        jdbcTemplate.update(sql, dc.getRate(), dc.getDiscountCode());
+    }
+
+    @Override
+    public void deleteDiscountCode(String code) {
+        String sql = "DELETE FROM DISCOUNT_CODE WHERE DISCOUNT_CODE = ?";
+        jdbcTemplate.update(sql, code);
+    }
+
+    @Override
     public List<MicroMarket> queryMicroMarket() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    @Override
+    public List<Customer> queryCustomer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
--- src/main/java/com/web/mvc/repository/spec/DiscountCodeDao.java
+++ src/main/java/com/web/mvc/repository/spec/DiscountCodeDao.java
@@ -6,4 +6,9 @@ import java.util.List;
 public interface DiscountCodeDao {
     // 相關 Discount Code CRUD 方法簽章
     List<DiscountCode> queryDiscountCode();
+    DiscountCode getDiscountCode(String code);
+    void saveDiscountCode(DiscountCode dc);
+    void updateDiscountCode(DiscountCode dc);
+    void deleteDiscountCode(String code);
+    
 }
