3b7ce1890363b1fd70232123a77f26c9e84f1e5e
MB-Teacher <MB-Teacher@MB-207-teacher>
2020-01-02 21:11:20 +0800

    jdbc

--- src/main/java/com/web/mvc/controller/DiscountCodeController.java
+++ src/main/java/com/web/mvc/controller/DiscountCodeController.java
@@ -3,10 +3,16 @@ package com.web.mvc.controller;
 
 import com.web.mvc.entity.DiscountCode;
 import com.web.mvc.repository.spec.CustomerDao;
+import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,11 +23,38 @@ public class DiscountCodeController {
     @Qualifier("customerDao")
     CustomerDao dao;
     
-    @RequestMapping("/input")
+    @GetMapping("/input")
     public String input(Model model) {
         model.addAttribute("po", new DiscountCode());
         model.addAttribute("list", dao.queryDiscountCode());
         return "discount_code";
     }
+    
+    @GetMapping("/{code}")
+    public String get(@PathParam("code") String code, Model model) {
+        DiscountCode dc = dao.getDiscountCode(code);
+        model.addAttribute("po", dc);
+        model.addAttribute("list", dao.queryDiscountCode());
+        return "discount_code";
+    }
+    
+    @PostMapping("/")
+    public String save(@ModelAttribute DiscountCode dc) {
+        dao.saveDiscountCode(dc);
+        return "redirect: ./input";
+    }
+    
+    @PutMapping("/{code}")
+    public String update(@PathParam("code") String code, @ModelAttribute DiscountCode dc) {
+        dao.updateDiscountCode(dc);
+        return "redirect: ./input";
+    }
+    
+    @DeleteMapping("/{code}")
+    public String delete(@PathParam("code") String code) {
+        dao.deleteDiscountCode(code);
+        return "redirect: ./input";
+    }
+    
     
 }
